4565887e23c0ec954c45b02da8676783e7ddd7dd
Nick Thorpe <nickthorpe71@gmail.com>
2021-07-15 17:42:27 -0500

    add base template for game scene

--- src/index.js
+++ src/index.js
@@ -1,39 +1,16 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
-
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
-
-    preload ()
-    {
-        this.load.image('logo', logoImg);
-    }
-      
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
-}
+import Game from './scenes/game';
+import Title from './scenes/title';
 
 const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
+    width: 1280,
+    height: 780,
+    scene: [
+        Game,
+        Title
+    ]
 };
 
 const game = new Phaser.Game(config);
---
+++ src/scenes/game.js
@@ -0,0 +1,19 @@
+export default class Game extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'Game'
+        });
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+        
+    }
+}
