220f03df0107f693bfc651067da9de29e1c6e483
Nick Thorpe <nickthorpe71@gmail.com>
2021-07-15 19:27:21 -0500

    add card object

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,3 +1,5 @@
+import { GameObjects } from "phaser";
+
 export default class Game extends Phaser.Scene {
     constructor() {
         super({
@@ -6,14 +8,44 @@ export default class Game extends Phaser.Scene {
     }
 
     preload() {
-
+        this.load.image('cyanCardFront', 'src/assets/cyanCardFront.png');
+        this.load.image('cyanCardBack', 'src/assets/cyanCardBack.png');
+        this.load.image('magentaCardFront', 'src/assets/magentaCardFront.png');
+        this.load.image('magentaCardBack', 'src/assets/magentaCardBack.png');
     }
 
     create() {
+        let self = this;
 
+        this.card = this.add.image(300, 300, 'cyanCardFront').setScale(0.3, 0.3).setInteractive();
+
+        this.input.setDraggable(this.card);
+
+        this.dealCards = () => {
+
+        }
+
+        this.dealText = this.add.text(75, 350, ['Deal Cards']).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff').setInteractive();
+
+        this.dealText.on('pointerdown', () => {
+            self.dealCards();
+        });
+
+        this.dealText.on('pointerover', () => {
+            self.dealText.setColor('#ff69bf');
+        });
+
+        this.dealText.on('pointerout', () => {
+            self.dealText.setColor('#00ffff');
+        });
+
+        this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+        });
     }
 
-    update() {
-        
-    }
+    // update() {
+
+    // }
 }
